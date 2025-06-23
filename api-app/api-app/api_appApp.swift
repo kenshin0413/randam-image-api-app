bc8d53367232698add2091169448b6d5e8ea5a98
kenshin0413 <kenshin1234569@gmail.com>
2025-06-23 21:28:35 +0900

    初期

---
+++ api-app/api-app/api_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_appApp.swift
+//  api-app
+//
+//  Created by miyamotokenshin on R 7/06/23.
+//
+
+import SwiftUI
+
+@main
+struct api_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
