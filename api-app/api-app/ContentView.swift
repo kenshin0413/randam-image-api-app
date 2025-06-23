5122e477e14e31071e6fe7d783a96497403602f6
kenshin0413 <kenshin1234569@gmail.com>
2025-06-23 23:43:41 +0900

    API を叩いて JSON の結果をコンソールに出力する

--- api-app/api-app/ContentView.swift
+++ api-app/api-app/ContentView.swift
@@ -10,12 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button(action: {
+                Task {
+                    await getData()
+                }
+            }, label: {
+                Text("取得")
+            })
         }
         .padding()
+    }
+    // https://note.com/dngri/n/n98ea0016049c
+    func getData() async {
+        do {
+            guard let url = URL(string: "https://picsum.photos/v2/list") else { return }
+            let (data, _) = try await URLSession.shared.data(from: url)
+            // dataをString型に変換する
+            // https://qiita.com/SNQ-2001/items/41efeb0a9fb4fa898177
+            let dataString = String(data: data, encoding: .utf8)
+            print(dataString as Any)
+        } catch {
+            print("Error")
+        }
     }
 }
 
